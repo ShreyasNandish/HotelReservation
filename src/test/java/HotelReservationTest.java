0870db6c3c2640b22c13bf5748d255b1847367a2
ShreyasNandish <shreyasnandish4@gmail.com>
2022-08-15 11:34:31 +0530

    Updated code to check the cheapest hotel

--- src/test/java/HotelReservationTest.java
+++ src/test/java/HotelReservationTest.java
@@ -8,11 +8,20 @@ public class HotelReservationTest {
     @BeforeAll
     static void initialize() {
         hotelReservation = new HotelReservationService();
+        hotelReservation.addHotel(new Hotel("Lakewood", 110));
+        hotelReservation.addHotel(new Hotel("Bridgewood", 160));
+        hotelReservation.addHotel(new Hotel("Ridgewood", 220));
     }
 
     @Test
     void givenHotel_ShouldBe_AddedToHotelList() {
         boolean result = hotelReservation.addHotel(new Hotel("Lakewood",110));
         Assertions.assertTrue(result);
+    }
+
+    @Test
+    void givenCheckInAndCheckOutDates_userShould_GetCheapestHotel() {
+        Hotel hotel = hotelReservation.getCheapestHotel("01-08-2022", "10-08-2022");
+        Assertions.assertEquals("Lakewood", hotel.getName());
     }
 }
